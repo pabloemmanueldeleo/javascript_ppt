45c61369619bf9e3ea3ffa2b0fdbbba2595e29e8
pabloemmanueldeleo <pabloemmanueldeleo@gmail.com>
2017-06-10 14:14:56 -0300

    agregado de css y html

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function numeroPar() {
-	var numero = document.getElementById('numero').value;
+'use strict';
 
-	var salida = document.getElementById('salida');
+function numeroPar () {
+	var numero = document.getElementById('numero').value
+
+	var salida = document.getElementById('salida')
 
 	if (isNaN(numero)){
-			salida.textContent = 'Tiene que ser un numero.';
+			salida.textContent = 'Tiene que ser un numero.'
 	}else{
 		var pares=[];
-		for (var i = 1; i < numero.length; i++) {
+		for (var i = 1; i <= numero; i++) {
 			if ( i % 2 === 0){
 				pares.push(i);
 			}
@@ -16,6 +18,7 @@ function numeroPar() {
 
 	}
 }
+
 function numeroAleatorio(min, max) {
 	var salida = document.getElementById('numeroAleatorio');
 	var puntaje = document.getElementById('puntaje');
@@ -41,5 +44,19 @@ function numeroAleatorio(min, max) {
 	}
 }
 function ppt(ppt) {
+	var compu=document.getElementById('compu');
+	var resultado=document.getElementById('resultado');
+	var elementos=['piedra','papel','tijera'];
+
+	var num = Math.round(Math.random() * (2 - 0) + 0);
+	compu.textContent = 'Compu: ('+ elementos[num] + ') Vs ' + ' (' + ppt + ')';
+	if(elementos[num] === ppt) {
+			resultado.textContent ='Resultado: Empataron.';
+		}
+	else if (elementos[num] == 'tijera' && ppt == 'papel' || elementos[num] == 'papel' && ppt == 'piedra' || elementos[num] == 'piedra' && ppt == 'tijera') {
+		resultado.textContent ='Resultado: Perdistes.';
+	}else {
+		resultado.textContent ='Resultado: GANASTES, CAPO.';
+	}
 
 }
